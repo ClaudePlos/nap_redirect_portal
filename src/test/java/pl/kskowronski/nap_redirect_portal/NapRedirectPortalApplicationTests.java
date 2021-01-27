9c8e814feadf8e0465a0cb15b889aafd643f031b
Klaudiusz Skowroński <claude-plos@o2.pl>
2021-01-27 13:48:47 +0100

    Initial commit

---
+++ src/test/java/pl/kskowronski/nap_redirect_portal/NapRedirectPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kskowronski.nap_redirect_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapRedirectPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
